7e0fcbdc25a24e7083ea0ca6a33ef777b7fbbe19
wanglz <547966213@qq.com>
2022-04-11 22:21:18 +0800

    xlua接口学习

---
+++ Assets/Scripts/5.CSharpCallLuaTable/CSharpCallLuaTableTest2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class CSharpCallLuaTableTest2 : MonoBehaviour
+{
+    void Start()
+    {
+        GlobalHelper.luaEnv.DoString("require 'File'");
+        IPerson _iperson = GlobalHelper.luaEnv.Global.Get<IPerson>("t1");
+        Debug.Log("name:" + _iperson.name);
+        Debug.Log("age:" + _iperson.age);
+        _iperson.mm();
+    }
+
+    [CSharpCallLua]
+    public interface IPerson
+    { 
+        string name { get; set; }
+        int age { get; set; }
+        void mm();
+    }
+}
