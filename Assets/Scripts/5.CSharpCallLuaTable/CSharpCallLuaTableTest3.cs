59982dcb74ae4c5e0eb5ea4cd7b0b2e30c9f0660
wanglz <547966213@qq.com>
2022-04-12 21:38:36 +0800

    list和dictionary访问lua中的table

---
+++ Assets/Scripts/5.CSharpCallLuaTable/CSharpCallLuaTableTest3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpCallLuaTableTest3 : MonoBehaviour
+{
+    private void Start()
+    {
+        GlobalHelper.luaEnv.DoString("require 'File'");
+        List<object> list = GlobalHelper.luaEnv.Global.Get<List<object>>("t1");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Debug.Log(list[i].ToString());        
+        }
+        Debug.Log("----------------------");
+        Dictionary<string, object> dic = GlobalHelper.luaEnv.Global.Get<Dictionary<string, object>>("t1");
+        foreach (var item in dic)
+        {
+            Debug.Log(item.Key + ":" + item.Value.ToString());
+        }
+    }
+}
