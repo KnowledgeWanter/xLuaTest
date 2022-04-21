d231e24ec1b8d67ce27bcee6b19006f43294e151
wanglz <547966213@qq.com>
2022-04-21 21:35:50 +0800

    LuaCallComponent

---
+++ Assets/Scripts/8.LuaCallComponent/LuaCallComponentTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuaCallComponentTest : MonoBehaviour
+{
+    void Start()
+    {
+        GlobalHelper.luaEnv.DoString("require 'File3'");
+
+        //GameObject objLight = GameObject.Find("Directional Light");
+        //Light _light = objLight.GetComponent<Light>();
+        //GameObject.Destroy(_light);
+    }
+}
